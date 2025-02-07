fe2237118589ce37a942add461c171490592bc56
Soumya <soumya@Soumyas-MacBook-Air.local>
2025-02-07 10:23:26 +0530

    step 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:rxddartexamples/views/homepage.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:developer' as devtools show log ;
+extension Log on Object{
+void log () => devtools.log(toString());
+}
 
 void main() {
   runApp(const MyApp());
+}
+
+void testit ()async{
+ final stream1 =  Stream.periodic(Duration(seconds: 1),(count) => 'Stream1,count:$count');
+ final stream2 = Stream.periodic(Duration(seconds: 3),(count) => 'Stream2,count:$count');
+final combined =  Rx.combineLatest2(stream1, stream2, (one,two) => 'One is $one,Two is $two');
+await for (final value in combined){
+  print(value);
+value.log();
+}
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +24,8 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
+    testit();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -22,4 +37,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Simple Scaffold App")),
+        body: Center(child: Text("Hello, Flutter!")),
+      ),
+    );
+  }
+}
